585e98036c99224e2ed25cc06109665f1bee245f
Joshua Kuan <jckuan1@uci.edu>
2023-04-21 23:59:19 -0400

    bugfix: update tz

--- util/static.ts
+++ util/static.ts
@@ -1,8 +1,8 @@
 import { RegionKey } from './types/types'
 
 export const RegionTimeZoneMapping: { [K in RegionKey]: string } = {
-  'US West': 'UTC-8',
-  'US East': 'UTC-5',
+  'US West': 'UTC-7',
+  'US East': 'UTC-4',
   'EU Central': 'UTC+1',
   'EU West': 'UTC-0',
   'South America': 'UTC-4',
