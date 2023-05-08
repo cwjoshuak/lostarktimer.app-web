55307e34a7532f8a66c3372aecb27f54c18e342b
alairon <alairon@outlook.com>
2023-05-08 16:15:48 -0400

    fix: improved resilience to bad data

--- bin/AutoScraper.js
+++ bin/AutoScraper.js
@@ -27,14 +27,24 @@ var AutoScraper = /** @class */ (function () {
             .replace(/.*var calendar_data=/i, '')
             .replace(/(?<=;).*/i, '')
             .replace(';', '');
+        try {
+            JSON.parse(this.buffer_data);
+        }
+        catch (e) {
+            this.buffer_data = '{}';
+        }
     };
     // Removes duplicates from entries
     AutoScraper.cleanCalendarData = function () {
         var _this = this;
         this.calendar_data = JSON.parse(this.buffer_data);
-        Object.keys(this.calendar_data).forEach(function (key) { return Object.keys(_this.calendar_data[key]).forEach(function (month) { return Object.keys(_this.calendar_data[key][month]).forEach(function (day) { return Object.keys(_this.calendar_data[key][month][day]).forEach(function (event) { return Object.keys(_this.calendar_data[key][month][day][event]).map(function (id) { return _this.calendar_data[key][month][day][event][id] = Array.from(new Set(_this.calendar_data[key][month][day][event][id])); }); }); }); }); });
+        Object.keys(this.calendar_data).forEach(function (type) { return Object.keys(_this.calendar_data[type]).forEach(function (month) { return Object.keys(_this.calendar_data[type][month]).forEach(function (day) { return Object.keys(_this.calendar_data[type][month][day]).forEach(function (event) { return Object.keys(_this.calendar_data[type][month][day][event]).forEach(
+        // Overwrite current array with one without duplicates
+        function (id) { return _this.calendar_data[type][month][day][event][id] = Array.from(new Set(_this.calendar_data[type][month][day][event][id])); }); }); }); }); });
     };
+    // Backs up the previous data file, then replaces it
     AutoScraper.writeData = function () {
+        fs.renameSync('./data/data.json', './data/data.old.json');
         fs.writeFileSync('./data/data.json', JSON.stringify(this.calendar_data), 'utf8');
     };
     AutoScraper.calendarURL = 'https://lostarkcodex.com/us/eventcalendar/';
--- bin/AutoScraper.ts
+++ bin/AutoScraper.ts
@@ -1,8 +1,8 @@
-const fs = require('fs');
-const https = require('https');
+const fs = require('fs')
+const https = require('https')
 
 interface CalendarData {
-  [key: string]: {
+  [type: string]: {
     [month: string]: {
       [day: string]: {
         [eventNo: string]: {
@@ -14,42 +14,50 @@ interface CalendarData {
 }
 
 export class AutoScraper {
-  private static readonly calendarURL: string = 'https://lostarkcodex.com/us/eventcalendar/';
-  private static buffer_data: string = '';
-  private static calendar_data: CalendarData = {};
+  private static readonly calendarURL: string = 'https://lostarkcodex.com/us/eventcalendar/'
+  private static buffer_data: string = ''
+  private static calendar_data: CalendarData = {}
 
-  public static scrapeCalendarData() {
+  public static scrapeCalendarData(): void {
     https.get(this.calendarURL, (res: any) => {
-      res.setEncoding('utf8');
+      res.setEncoding('utf8')
       res.on('data', (chunk: string) => {
-        this.buffer_data += chunk;
-      });
+        this.buffer_data += chunk
+      })
       res.on('end', () => {
-        this.extractCalendarData();
-        this.cleanCalendarData();
-        this.writeData();
-      });
-    });
+        this.extractCalendarData()
+        this.cleanCalendarData()
+        this.writeData()
+      })
+    })
   }
 
   // Strip everything except the calendar_data variable
-  private static extractCalendarData() {
+  private static extractCalendarData(): void {
     this.buffer_data = this.buffer_data
-    .replace(/\r\n/gmi, ' ')
-    .replace(/.*var calendar_data=/i, '')
-    .replace(/(?<=;).*/i, '')
-    .replace(';', '');
+      .replace(/\r\n/gmi, ' ')
+      .replace(/.*var calendar_data=/i, '')
+      .replace(/(?<=;).*/i, '')
+      .replace(';', '')
+
+    try {
+      JSON.parse(this.buffer_data)
+    } catch (err: unknown) {
+      console.error(err);
+      this.buffer_data = '{}'
+    }
   }
 
   // Removes duplicates from entries
-  private static cleanCalendarData() {
-    this.calendar_data = JSON.parse(this.buffer_data);
+  private static cleanCalendarData(): void {
+    this.calendar_data = JSON.parse(this.buffer_data)
     Object.keys(this.calendar_data).forEach(
-      key => Object.keys(this.calendar_data[key]).forEach(
-        month => Object.keys(this.calendar_data[key][month]).forEach(
-          day => Object.keys(this.calendar_data[key][month][day]).forEach(
-            event => Object.keys(this.calendar_data[key][month][day][event]).map(
-              id => this.calendar_data[key][month][day][event][id] = Array.from(new Set(this.calendar_data[key][month][day][event][id]))
+      type => Object.keys(this.calendar_data[type]).forEach(
+        month => Object.keys(this.calendar_data[type][month]).forEach(
+          day => Object.keys(this.calendar_data[type][month][day]).forEach(
+            event => Object.keys(this.calendar_data[type][month][day][event]).forEach(
+              // Overwrite current array with one without duplicates
+              id => this.calendar_data[type][month][day][event][id] = Array.from(new Set(this.calendar_data[type][month][day][event][id]))
             )
           )
         )
@@ -57,9 +65,13 @@ export class AutoScraper {
     )
   }
 
-  private static writeData() {
-    fs.writeFileSync('./data/data.json', JSON.stringify(this.calendar_data), 'utf8');
+  // If data was successfully formed, backs up the previous data file, then creates a new data file
+  private static writeData(): void {
+    if (Object.values(this.calendar_data) !== Object.values({})) {
+      fs.renameSync('./data/data.json', './data/data.old.json')
+      fs.writeFileSync('./data/data.json', JSON.stringify(this.calendar_data), 'utf8')
+    }
   }
 }
 
-module.exports = AutoScraper;
+module.exports = AutoScraper
