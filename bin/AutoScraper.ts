4ad10919e86c7434711ccc7e972104adf8e9b425
Ambrose Lai <alairon@outlook.com>
2023-05-08 23:59:01 -0400

    fix: error checking with empty data

--- bin/AutoScraper.ts
+++ bin/AutoScraper.ts
@@ -15,14 +15,14 @@ interface CalendarData {
 
 export class AutoScraper {
   private static readonly calendarURL: string = 'https://lostarkcodex.com/us/eventcalendar/'
-  private static buffer_data: string = ''
-  private static calendar_data: CalendarData = {}
+  private static bufferData: string = ''
+  private static calendarData: CalendarData = {}
 
   public static scrapeCalendarData(): void {
     https.get(this.calendarURL, (res: any) => {
       res.setEncoding('utf8')
       res.on('data', (chunk: string) => {
-        this.buffer_data += chunk
+        this.bufferData += chunk
       })
       res.on('end', () => {
         this.extractCalendarData()
@@ -34,30 +34,30 @@ export class AutoScraper {
 
   // Strip everything except the calendar_data variable
   private static extractCalendarData(): void {
-    this.buffer_data = this.buffer_data
+    this.bufferData = this.bufferData
       .replace(/\r\n/gmi, ' ')
       .replace(/.*var calendar_data=/i, '')
       .replace(/(?<=;).*/i, '')
       .replace(';', '')
 
     try {
-      JSON.parse(this.buffer_data)
+      JSON.parse(this.bufferData)
     } catch (err: unknown) {
       console.error(err);
-      this.buffer_data = '{}'
+      this.bufferData = '{}'
     }
   }
 
   // Removes duplicates from entries
   private static cleanCalendarData(): void {
-    this.calendar_data = JSON.parse(this.buffer_data)
-    Object.keys(this.calendar_data).forEach(
-      type => Object.keys(this.calendar_data[type]).forEach(
-        month => Object.keys(this.calendar_data[type][month]).forEach(
-          day => Object.keys(this.calendar_data[type][month][day]).forEach(
-            event => Object.keys(this.calendar_data[type][month][day][event]).forEach(
+    this.calendarData = JSON.parse(this.bufferData)
+    Object.keys(this.calendarData).forEach(
+      type => Object.keys(this.calendarData[type]).forEach(
+        month => Object.keys(this.calendarData[type][month]).forEach(
+          day => Object.keys(this.calendarData[type][month][day]).forEach(
+            event => Object.keys(this.calendarData[type][month][day][event]).forEach(
               // Overwrite current array with one without duplicates
-              id => this.calendar_data[type][month][day][event][id] = Array.from(new Set(this.calendar_data[type][month][day][event][id]))
+              id => this.calendarData[type][month][day][event][id] = Array.from(new Set(this.calendarData[type][month][day][event][id]))
             )
           )
         )
@@ -67,9 +67,11 @@ export class AutoScraper {
 
   // If data was successfully formed, backs up the previous data file, then creates a new data file
   private static writeData(): void {
-    if (Object.values(this.calendar_data) !== Object.values({})) {
-      fs.renameSync('./data/data.json', './data/data.old.json')
-      fs.writeFileSync('./data/data.json', JSON.stringify(this.calendar_data), 'utf8')
+    if (Object.values(this.calendarData).length > 0) {
+      fs.rename('./data/data.json', './data/data.old.json', (err: Error) => {
+        if (err) console.error(err)
+        fs.writeFileSync('./data/data.json', JSON.stringify(this.calendarData), 'utf8')
+      })
     }
   }
 }
